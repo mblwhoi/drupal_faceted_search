7a389662bbe0916b583da4ca0b180bea02fe9e80
David Lesieur <david@davidlesieur.com>
2007-07-29 03:47:22 +0000

    Added client caching for tooltips. Changed tooltip data to JSON.

--- jquery.faceted_search_ui.js
+++ jquery.faceted_search_ui.js
@@ -4,37 +4,35 @@
  * Provides tooltips with subcategories when hovering categories, through AHAH.
  */ 
 jQuery.facetedSearchUI = {
-  isActive : false,
+  isActive : false, // True when hovering a category.
+  cache : {}, // Cached HTML chunks.
+
   activate : function() {
     if (jQuery.facetedSearchUI.isActive || !this.href) {
       return;
     }
     
-    // Extract the url to load from the category's url.
-    var url = null;
+    // Extract the facet key and id from the class of the category's parent.
     var facetKey = null;
     var facetId = null;
+    
     jQuery(this).parents('.faceted-search-facet').each(function() {
-      // Extract the facet key and id from the class of the category's parent.
       var matches = jQuery(this).attr('class').match(/faceted-search-facet--([^ ]+)--([^ ]+)/);
       if (matches.length > 0) {
         facetKey = matches[1];
         facetId = matches[2];
       }
     });
-    if (facetKey && facetId) {
-      // Derive the url from the category's url, which also contains the search text.
-      url = this.href.replace(/\/results\/na\//, '/categories/' + facetKey + '%3A' + facetId + '/');
+    if (!facetKey || !facetId) {
+      return;
     }
 
-    // If we've been able to extract an url, perform the request.
+    // Derive the url from the category's url, which also contains the search text.
+    var url = this.href.replace(/\/results\/na\//, '/categories/' + facetKey + '%3A' + facetId + '/');
     if (url != this.href) {
       jQuery.facetedSearchUI.isActive = true;
       
-      // Request HTML chunk for the tooltip.
-      jQuery.get(url, null, jQuery.facetedSearchUI.show);
-
-      // Prepare the tooptip
+      // Prepare the tooptip.
       var elemPosition = jQuery(this).offset();
       var elemWidth = jQuery(this).outerWidth();
       var windowWidth = jQuery(window).width();
@@ -49,20 +47,45 @@ jQuery.facetedSearchUI = {
         tooltip.css('left', (elemPosition.left + elemWidth + 10) + 'px');
         tooltip.addClass('faceted-search-tooltip-right'); // Might be useful for theming.
       }
+
+      // Show the tooltip.
+      var id = jQuery.facetedSearchUI.makeCacheId(url);
+      if (id in jQuery.facetedSearchUI.cache) { // Show from the cache.
+        jQuery.facetedSearchUI.show(jQuery.facetedSearchUI.cache[id]);
+      }
+      else { // Was not in the cache, load it from the server.
+        jQuery.get(url, null, jQuery.facetedSearchUI.load);
+      }
     }
   },
+  
   deactivate : function() {
     if (jQuery.facetedSearchUI.isActive) {
       jQuery.facetedSearchUI.isActive = false;
       jQuery('#faceted-search-tooltip').hide().empty().removeClass('faceted-search-tooltip-left').removeClass('faceted-search-tooltip-right');
     }
   },
+
+  load : function(data) {
+    data = Drupal.parseJson(data);
+    if (data.id) {
+      // Cache the received HTML chunk and show it.
+      jQuery.facetedSearchUI.cache[data.id] = data.content;
+      jQuery.facetedSearchUI.show(data.content);
+    }
+  },
+  
   show : function(chunk) {
-    // Avoid showing the chunk if it's too late (e.g. after the mouse has moved
-    // out of the category).
+    // Show the tooltip, but only if we were called back while the tooltip was
+    // still active (i.e. only if the mouse did not move out).
     if (jQuery.facetedSearchUI.isActive && chunk.length > 0) {
       jQuery('#faceted-search-tooltip').empty().append(chunk).show();
     }
+  },
+
+  makeCacheId : function(url) {
+    // Extract the search text to use as id.
+    return url.substr(url.lastIndexOf('/') + 1);
   }
 };
 
